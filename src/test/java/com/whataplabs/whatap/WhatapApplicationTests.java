37c793cf95a27ae92c4d621d57ed2c62016582b2
kim1387 <kevink0@naver.com>
2022-12-08 21:27:26 +0900

    Initial commit

---
+++ src/test/java/com/whataplabs/whatap/WhatapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whataplabs.whatap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
