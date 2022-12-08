75a5d7ae58f7c005573ddf7b0ba66cc9e4652ac6
kim1387 <kevink0@naver.com>
2022-12-08 22:17:01 +0900

    chore: add swagger setting

---
+++ src/main/java/com/whataplabs/whatap/global/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.whataplabs.whatap.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI whaTapOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("WhaTap 1차 과제 API").description("WhaTap 1차 과제 API 입니다.").version("v0.0.1"));
+  }
+}
