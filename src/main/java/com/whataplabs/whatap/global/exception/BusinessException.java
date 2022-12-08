58b89c7c0347b5ccd32f1c73e6bc656e39122343
kim1387 <kevink0@naver.com>
2022-12-08 22:34:23 +0900

    feat: 에러를 위한 상위 클래스 제작

---
+++ src/main/java/com/whataplabs/whatap/global/exception/BusinessException.java
@@ -0,0 +1,16 @@
+package com.whataplabs.whatap.global.exception;
+
+
+import com.whataplabs.whatap.global.response.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+
+  public BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+}
