013f9060531347a77b9c0d5fe3632ea428f6d18a
kim1387 <kevink0@naver.com>
2022-12-09 14:40:39 +0900

    chore: set JpaAuditing config

---
+++ src/main/java/com/whataplabs/whatap/global/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.whataplabs.whatap.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+
+}
