11293c26535ae2bed1f4ac74bcdc6256d324b43e
kim1387 <kevink0@naver.com>
2022-12-09 17:13:35 +0900

    feat: product 생성 함수 통합 테스트 작성
    - 통합 테스트 작성
    - restdocs를 활용한 api 문서화

---
+++ src/test/java/com/whataplabs/whatap/domain/product/controller/ProductControllerTest.java
@@ -0,0 +1,75 @@
+package com.whataplabs.whatap.domain.product.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.whataplabs.whatap.domain.product.ProductFixtures;
+import com.whataplabs.whatap.domain.product.controller.document.ProductRestDocument;
+import com.whataplabs.whatap.domain.product.dto.ProductInfo;
+import com.whataplabs.whatap.domain.product.dto.ProductRegisterRequest;
+import com.whataplabs.whatap.domain.product.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.whataplabs.whatap.domain.product.ProductFixtures.PRODUCT_ONE_REGISTER_REQUEST;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(controllers = ProductController.class)
+class ProductControllerTest {
+  @MockBean private ProductService productService;
+
+  private ObjectMapper objectMapper;
+
+  private MockMvc mockMvc;
+
+  @BeforeEach
+  void setUp(
+      WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentationContextProvider) {
+    objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    mockMvc =
+        MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
+            .apply(documentationConfiguration(restDocumentationContextProvider))
+            .build();
+  }
+
+  @Test
+  @DisplayName("product 등록 api")
+  void registerCity() throws Exception {
+    // given
+    ProductInfo productInfo = ProductFixtures.PRODUCT_ONE_INFO;
+    ProductRegisterRequest cityRegisterRequest = PRODUCT_ONE_REGISTER_REQUEST;
+    // when
+    when(productService.registerProduct(any())).thenReturn(productInfo);
+
+    // then
+    mockMvc
+        .perform(
+            RestDocumentationRequestBuilders.post("/api/v1/product")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(cityRegisterRequest)))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andDo(ProductRestDocument.getCreateProductInfoDocument());
+  }
+}
---
+++ src/test/java/com/whataplabs/whatap/domain/product/controller/document/ProductRestDocument.java
@@ -0,0 +1,33 @@
+package com.whataplabs.whatap.domain.product.controller.document;
+
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+
+public class ProductRestDocument {
+  public static RestDocumentationResultHandler getCreateProductInfoDocument() {
+    return document(
+        "/travel/create",
+        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+        requestFields(
+            fieldWithPath("productName").type(JsonFieldType.STRING).description("product 이름"),
+            fieldWithPath("productIntro").type(JsonFieldType.STRING).description("product 소개"),
+            fieldWithPath("price").type(JsonFieldType.NUMBER).description("product 가격")),
+        responseFields(
+            fieldWithPath("code").type(JsonFieldType.STRING).description("Business code"),
+            fieldWithPath("message").type(JsonFieldType.STRING).description("response message"),
+            fieldWithPath("data.productName").type(JsonFieldType.STRING).description("product 이름"),
+            fieldWithPath("data.productIntro")
+                .type(JsonFieldType.STRING)
+                .description("product 에 대한 소개"),
+            fieldWithPath("data.price").type(JsonFieldType.NUMBER).description("product 가격"),
+            fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("product 생성 일자"),
+            fieldWithPath("data.updatedAt")
+                .type(JsonFieldType.STRING)
+                .description("product 내용 수정 일자")));
+  }
+}
